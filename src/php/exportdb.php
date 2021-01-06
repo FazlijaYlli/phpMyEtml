2c36509e9e59dc5161ade684492bb6a390e3bf7e
Elias <elias.vuille@eduvaud.ch>
2021-01-06 13:14:20 +0100

    Changed filename export

--- src/php/exportdb.php
+++ src/php/exportdb.php
@@ -1,6 +1,7 @@
 <?php
+session_start();
 $userprofile = getenv('USERPROFILE');
-$command = "F:\Logiciels\Wamp\UwAmp\bin\database\mysql-5.7.11\bin\mysqldump --user=root --password=root --host=localhost ".$_GET['dbList']." > ".$userprofile."/Downloads/".$_GET['dbList']."_backup.sql";
+$command = "F:\Logiciels\Wamp\UwAmp\bin\database\mysql-5.7.11\bin\mysqldump --user=root --password=root --host=localhost ".$_GET['dbList']." > ".$userprofile."/Downloads/".$_GET['dbList']."_".$_SESSION["user"].".sql";
 
 
 $return_var = NULL;
